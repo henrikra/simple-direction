62e29e736d1161603558b32ecbbd079c1facc8ef
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-07-26 18:57:22 +0300

    Remove demo stuff

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import {
   Text,
   View,
   StatusBar,
-  ScrollView,
+  Button,
 } from 'react-native';
 import Mapbox, { MapView } from 'react-native-mapbox-gl';
 
@@ -25,7 +25,6 @@ export default class simpleDirections extends Component {
 
   onRegionDidChange = (location) => {
     this.setState({ currentZoom: location.zoomLevel });
-    this.getDirections(undefined, {latitude: location.latitude, longitude: location.longitude})
   };
   onRegionWillChange = (location) => {
     // console.log('onRegionWillChange', location);
@@ -62,31 +61,33 @@ export default class simpleDirections extends Component {
     });
   }
 
-  getDirections = (from = {latitude: 60.162059, longitude: 24.94}, to = {latitude: 60.152059, longitude: 24.92}) => {
-    fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${from.longitude}%2C${from.latitude}%3B${to.longitude}%2C${to.latitude}.json?access_token=pk.eyJ1IjoiaGVucmlrcmEiLCJhIjoiY2o1a3czMjA1MDlzejJwbXhpam1oMTJpMSJ9.TANJvIveftY7gEV8Um3Aew&geometries=geojson`)
-      .then((response) => response.json())
-      .then(res => {
-        console.log('res', res.routes[0].geometry.coordinates);
-        this.setState({
-          annotations: [ 
-            ...this.state.annotations,
-            {
-              coordinates: res.routes[0].geometry.coordinates.map(coordinates => coordinates.reverse()),
-              type: 'polyline',
-              id: 'new-black-polygon',
-              strokeWidth: 3,
-              strokeColor: '#FF0000',
-            },
-            {
-              coordinates: [60.162059, 24.94],
-              type: 'point',
-              title: 'This is a the lol marker',
-              id: 'foo'
-            }, 
-          ],
-        });
-      })
-      .catch(err => console.log('err', err))
+  getDirections = () => {
+    const from = {latitude: 60.162059, longitude: 24.94};
+    this._map.getCenterCoordinateZoomLevel(coordinates => {
+      fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${from.longitude}%2C${from.latitude}%3B${coordinates.longitude}%2C${coordinates.latitude}.json?access_token=pk.eyJ1IjoiaGVucmlrcmEiLCJhIjoiY2o1a3czMjA1MDlzejJwbXhpam1oMTJpMSJ9.TANJvIveftY7gEV8Um3Aew&geometries=geojson`)
+        .then((response) => response.json())
+        .then(res => {
+          this.setState({
+            annotations: [ 
+              ...this.state.annotations,
+              {
+                coordinates: res.routes[0].geometry.coordinates.map(coordinates => coordinates.reverse()),
+                type: 'polyline',
+                id: 'new-black-polygon',
+                strokeWidth: 3,
+                strokeColor: '#FF0000',
+              },
+              {
+                coordinates: [60.162059, 24.94],
+                type: 'point',
+                title: 'This is a the lol marker',
+                id: 'foo'
+              }, 
+            ],
+          });
+        })
+        .catch(err => console.log('err', err))
+    });
   }
 
   componentDidMount() {
@@ -153,114 +154,7 @@ export default class simpleDirections extends Component {
           onLongPress={this.onLongPress}
           onTap={this.onTap}
         />
-      <ScrollView style={styles.scrollView}>
-        {this._renderButtons()}
-      </ScrollView>
-      </View>
-    );
-  }
-
-  _renderButtons() {
-    return (
-      <View>
-        <Text onPress={() => this._map && this._map.setDirection(0)}>
-          Set direction to 0
-        </Text>
-        <Text onPress={() => this._map && this._map.setZoomLevel(6)}>
-          Zoom out to zoom level 6
-        </Text>
-        <Text onPress={() => this._map && this._map.setCenterCoordinate(48.8589, 2.3447)}>
-          Go to Paris at current zoom level {parseInt(this.state.currentZoom)}
-        </Text>
-        <Text onPress={() => this._map && this._map.setCenterCoordinateZoomLevel(35.68829, 139.77492, 14)}>
-          Go to Tokyo at fixed zoom level 14
-        </Text>
-        <Text onPress={() => this._map && this._map.easeTo({ pitch: 30 })}>
-          Set pitch to 30 degrees
-        </Text>
-        <Text onPress={this.getDirections} style={{backgroundColor: 'blue', padding: 20}}>
-          Add new marker
-        </Text>
-        <Text onPress={this.updateMarker2}>
-          Update marker2
-        </Text>
-        <Text onPress={() => this._map && this._map.selectAnnotation('marker1')}>
-          Open marker1 popup
-        </Text>
-        <Text onPress={() => this._map && this._map.deselectAnnotation()}>
-          Deselect annotation
-        </Text>
-        <Text onPress={this.removeMarker2}>
-          Remove marker2 annotation
-        </Text>
-        <Text onPress={() => this.setState({ annotations: [] })}>
-          Remove all annotations
-        </Text>
-        <Text onPress={() => this._map && this._map.setVisibleCoordinateBounds(40.712, -74.227, 40.774, -74.125, 100, 0, 0, 0)}>
-          Set visible bounds to 40.7, -74.2, 40.7, -74.1
-        </Text>
-        <Text onPress={() => this.setState({ userTrackingMode: Mapbox.userTrackingMode.followWithHeading })}>
-          Set userTrackingMode to followWithHeading
-        </Text>
-        <Text onPress={() => this._map && this._map.getCenterCoordinateZoomLevel((location)=> {
-            console.log(location);
-          })}>
-          Get location
-        </Text>
-        <Text onPress={() => this._map && this._map.getDirection((direction)=> {
-            console.log(direction);
-          })}>
-          Get direction
-        </Text>
-        <Text onPress={() => this._map && this._map.getBounds((bounds)=> {
-            console.log(bounds);
-          })}>
-          Get bounds
-        </Text>
-        <Text onPress={() => {
-            Mapbox.addOfflinePack({
-              name: 'test',
-              type: 'bbox',
-              bounds: [0, 0, 0, 0],
-              minZoomLevel: 0,
-              maxZoomLevel: 0,
-              metadata: { anyValue: 'you wish' },
-              styleURL: Mapbox.mapStyles.dark
-            }).then(() => {
-              console.log('Offline pack added');
-            }).catch(err => {
-              console.log(err);
-            });
-        }}>
-          Create offline pack
-        </Text>
-        <Text onPress={() => {
-            Mapbox.getOfflinePacks()
-              .then(packs => {
-                console.log(packs);
-              })
-              .catch(err => {
-                console.log(err);
-              });
-        }}>
-          Get offline packs
-        </Text>
-        <Text onPress={() => {
-            Mapbox.removeOfflinePack('test')
-              .then(info => {
-                if (info.deleted) {
-                  console.log('Deleted', info.deleted);
-                } else {
-                  console.log('No packs to delete');
-                }
-              })
-              .catch(err => {
-                console.log(err);
-              });
-        }}>
-          Remove pack with name 'test'
-        </Text>
-        <Text>User tracking mode is {this.state.userTrackingMode}</Text>
+        <Button title="Get directions" onPress={this.getDirections} />
       </View>
     );
   }
