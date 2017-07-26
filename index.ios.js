fa73638861f01a79466443f9283ded1f6539181f
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-07-26 15:20:32 +0300

    Add simple directions example

--- index.ios.js
+++ index.ios.js
@@ -21,92 +21,122 @@ Mapbox.setAccessToken(accessToken);
 export default class simpleDirections extends Component {
   state = {
     center: {
-      latitude: 40.72052634,
+      latitude: 40.74052634,
       longitude: -73.97686958312988
     },
-    zoom: 11,
+    zoom: 12,
     userTrackingMode: Mapbox.userTrackingMode.none,
-    annotations: [{
-      coordinates: [40.72052634, -73.97686958312988],
-      type: 'point',
-      title: 'This is marker 1',
-      subtitle: 'It has a rightCalloutAccessory too',
-      rightCalloutAccessory: {
-        source: { uri: 'https://cldup.com/9Lp0EaBw5s.png' },
-        height: 25,
-        width: 25
-      },
-      annotationImage: {
-        source: { uri: 'https://cldup.com/CnRLZem9k9.png' },
-        height: 25,
-        width: 25
-      },
-      id: 'marker1'
-    }, {
-      coordinates: [40.714541341726175,-74.00579452514648],
-      type: 'point',
-      title: 'Important!',
-      subtitle: 'Neat, this is a custom annotation image',
-      annotationImage: {
-        source: { uri: 'https://cldup.com/7NLZklp8zS.png' },
-        height: 25,
-        width: 25
-      },
-      id: 'marker2'
-    }, {
-      coordinates: [[40.76572150042782,-73.99429321289062],[40.743485405490695, -74.00218963623047],[40.728266950429735,-74.00218963623047],[40.728266950429735,-73.99154663085938],[40.73633186448861,-73.98983001708984],[40.74465591168391,-73.98914337158203],[40.749337730454826,-73.9870834350586]],
-      type: 'polyline',
-      strokeColor: '#00FB00',
-      strokeWidth: 4,
-      strokeAlpha: .5,
-      id: 'foobar'
-    }, {
-      coordinates: [[40.749857912194386, -73.96820068359375], [40.741924698522055,-73.9735221862793], [40.735681504432264,-73.97523880004883], [40.7315190495212,-73.97438049316406], [40.729177554196376,-73.97180557250975], [40.72345355209305,-73.97438049316406], [40.719290332250544,-73.97455215454102], [40.71369559554873,-73.97729873657227], [40.71200407096382,-73.97850036621094], [40.71031250340588,-73.98691177368163], [40.71031250340588,-73.99154663085938]],
-      type: 'polygon',
-      fillAlpha: 1,
-      strokeColor: '#ffffff',
-      fillColor: '#0000ff',
-      id: 'zap'
-    }]
+    annotations: [
+      // {
+      //   coordinates: [40.72052634, -73.97686958312988],
+      //   type: 'point',
+      //   title: 'This is marker 1',
+      //   subtitle: 'It has a rightCalloutAccessory too',
+      //   rightCalloutAccessory: {
+      //     source: { uri: 'https://cldup.com/9Lp0EaBw5s.png' },
+      //     height: 25,
+      //     width: 25
+      //   },
+      //   annotationImage: {
+      //     source: { uri: 'https://cldup.com/CnRLZem9k9.png' },
+      //     height: 25,
+      //     width: 25
+      //   },
+      //   id: 'marker1'
+      // }, 
+      // {
+      //   coordinates: [40.714541341726175,-74.00579452514648],
+      //   type: 'point',
+      //   title: 'Important!',
+      //   subtitle: 'Neat, this is a custom annotation image',
+      //   annotationImage: {
+      //     source: { uri: 'https://cldup.com/7NLZklp8zS.png' },
+      //     height: 25,
+      //     width: 25
+      //   },
+      //   id: 'marker2'
+      // }, 
+      // {
+      //   coordinates: [[40.76572150042782,-73.99429321289062],[40.743485405490695, -74.00218963623047],[40.728266950429735,-74.00218963623047],[40.728266950429735,-73.99154663085938],[40.73633186448861,-73.98983001708984],[40.74465591168391,-73.98914337158203],[40.749337730454826,-73.9870834350586]],
+      //   type: 'polyline',
+      //   strokeColor: '#00FB00',
+      //   strokeWidth: 4,
+      //   strokeAlpha: .5,
+      //   id: 'foobar'
+      // }, 
+      // {
+      //   coordinates: [[40.749857912194386, -73.96820068359375], [40.741924698522055,-73.9735221862793], [40.735681504432264,-73.97523880004883], [40.7315190495212,-73.97438049316406], [40.729177554196376,-73.97180557250975], [40.72345355209305,-73.97438049316406], [40.719290332250544,-73.97455215454102], [40.71369559554873,-73.97729873657227], [40.71200407096382,-73.97850036621094], [40.71031250340588,-73.98691177368163], [40.71031250340588,-73.99154663085938]],
+      //   type: 'polygon',
+      //   fillAlpha: 1,
+      //   strokeColor: '#ffffff',
+      //   fillColor: '#0000ff',
+      //   id: 'zap'
+      // }
+    ]
   };
 
   onRegionDidChange = (location) => {
     this.setState({ currentZoom: location.zoomLevel });
-    console.log('onRegionDidChange', location);
+    // console.log('onRegionDidChange', location);
   };
   onRegionWillChange = (location) => {
-    console.log('onRegionWillChange', location);
+    // console.log('onRegionWillChange', location);
   };
   onUpdateUserLocation = (location) => {
-    console.log('onUpdateUserLocation', location);
+    // console.log('onUpdateUserLocation', location);
   };
   onOpenAnnotation = (annotation) => {
-    console.log('onOpenAnnotation', annotation);
+    // console.log('onOpenAnnotation', annotation);
   };
   onRightAnnotationTapped = (e) => {
-    console.log('onRightAnnotationTapped', e);
+    // console.log('onRightAnnotationTapped', e);
   };
   onLongPress = (location) => {
-    console.log('onLongPress', location);
+    // console.log('onLongPress', location);
   };
   onTap = (location) => {
-    console.log('onTap', location);
+    // console.log('onTap', location);
   };
   onChangeUserTrackingMode = (userTrackingMode) => {
     this.setState({ userTrackingMode });
-    console.log('onChangeUserTrackingMode', userTrackingMode);
+    // console.log('onChangeUserTrackingMode', userTrackingMode);
   };
 
   componentWillMount() {
     this._offlineProgressSubscription = Mapbox.addOfflinePackProgressListener(progress => {
-      console.log('offline pack progress', progress);
+      // console.log('offline pack progress', progress);
     });
     this._offlineMaxTilesSubscription = Mapbox.addOfflineMaxAllowedTilesListener(tiles => {
-      console.log('offline max allowed tiles', tiles);
+      // console.log('offline max allowed tiles', tiles);
     });
     this._offlineErrorSubscription = Mapbox.addOfflineErrorListener(error => {
-      console.log('offline error', error);
+      // console.log('offline error', error);
     });
+  }
+
+  getDirections = () => {
+    console.log('painoin');
+    fetch('https://api.mapbox.com/directions/v5/mapbox/driving/-73.96820068359375%2C40.749857912194386%3B-74%2C40.733.json?access_token=pk.eyJ1IjoiaGVucmlrcmEiLCJhIjoiY2o1a3czMjA1MDlzejJwbXhpam1oMTJpMSJ9.TANJvIveftY7gEV8Um3Aew&geometries=geojson')
+      .then((response) => response.json())
+      .then(res => {
+        console.log('res', res.routes[0].geometry.coordinates);
+        this.setState({
+          annotations: [ 
+            ...this.state.annotations,
+            {
+              coordinates: res.routes[0].geometry.coordinates.map(coordinate => [coordinate[1], coordinate[0]]),
+              type: 'polyline',
+              id: 'new-black-polygon',
+              strokeWidth: 3,
+              strokeColor: '#FF0000',
+            }
+          ]
+        });
+      })
+      .catch(err => console.log('err', err))
+  }
+
+  componentDidMount() {
   }
 
   componentWillUnmount() {
@@ -116,22 +146,41 @@ export default class simpleDirections extends Component {
   }
 
   addNewMarkers = () => {
+    this.getDirections();
     // Treat annotations as immutable and create a new one instead of using .push()
-    this.setState({
-      annotations: [ ...this.state.annotations, {
-        coordinates: [40.73312,-73.989],
-        type: 'point',
-        title: 'This is a new marker',
-        id: 'foo'
-      }, {
-        'coordinates': [[40.749857912194386, -73.96820068359375], [40.741924698522055,-73.9735221862793], [40.735681504432264,-73.97523880004883], [40.7315190495212,-73.97438049316406], [40.729177554196376,-73.97180557250975], [40.72345355209305,-73.97438049316406], [40.719290332250544,-73.97455215454102], [40.71369559554873,-73.97729873657227], [40.71200407096382,-73.97850036621094], [40.71031250340588,-73.98691177368163], [40.71031250340588,-73.99154663085938]],
-        'type': 'polygon',
-        'fillAlpha': 1,
-        'fillColor': '#000000',
-        'strokeAlpha': 1,
-        'id': 'new-black-polygon'
-      }]
-    });
+    // this.setState({
+    //   annotations: [ 
+    //     ...this.state.annotations, 
+    //     // {
+    //     //   coordinates: [40.73312,-73.989],
+    //     //   type: 'point',
+    //     //   title: 'This is a the lol marker',
+    //     //   id: 'foo'
+    //     // }, 
+    //     {
+    //       'coordinates': [
+    //         [40.749857912194386, -73.96820068359375], 
+    //         [40.741924698522055,-73.9735221862793], 
+    //         [40.735681504432264,-73.97523880004883], 
+    //         [40.7315190495212,-73.97438049316406], 
+    //         [40.729177554196376,-73.97180557250975], 
+    //         [40.72345355209305,-73.97438049316406], 
+    //         [40.719290332250544,-73.97455215454102], 
+    //         [40.71369559554873,-73.97729873657227], 
+    //         [40.71200407096382,-73.97850036621094], 
+    //         [40.71031250340588,-73.98691177368163], 
+    //         [40.71031250340588,-73.99154663085938],
+    //       ],
+    //       'type': 'polygon',
+    //       // 'fillAlpha': 1,
+    //       // 'fillColor': '#ffffff',
+    //       'strokeAlpha': 1,
+    //       'id': 'new-black-polygon',
+    //       strokeWidth: 15,
+    //       strokeColor: '#FF0000',
+    //     }
+    //   ]
+    // });
   };
 
   updateMarker2 = () => {
@@ -213,7 +262,7 @@ export default class simpleDirections extends Component {
         <Text onPress={() => this._map && this._map.easeTo({ pitch: 30 })}>
           Set pitch to 30 degrees
         </Text>
-        <Text onPress={this.addNewMarkers}>
+        <Text onPress={this.getDirections} style={{backgroundColor: 'blue', padding: 20}}>
           Add new marker
         </Text>
         <Text onPress={this.updateMarker2}>
